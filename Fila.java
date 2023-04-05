755216b9bc4de89690d9b788ed979e02b37cc66a
Gustavo543210123 <126624859+Gustavo543210123@users.noreply.github.com>
2023-04-04 21:32:36 -0300

    Add files via upload

---
+++ Fila.java
@@ -0,0 +1,24 @@
+public class Fila {
+    private static Fila instancia;
+
+    public static Fila getInstance() {
+        if (instancia == null) {
+            instancia = new Fila();
+        }
+        return instancia;
+    }
+
+    private Fila() {
+    }
+
+    public void ImprimeDocumento(){
+
+    }
+
+    public void RemoveDocumento(){
+
+    }
+    public void RemoveTodosDocumentos(){
+
+    }
+}
